1b64210983949abbab91ab6769b6342ded1b5c0a
stillrdtr <stillrdtr@gmail.com>
2026-03-27 16:37:37 -0700

    a

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -8,9 +8,37 @@ module.exports = {
     const channel = member.guild.channels.cache.get(welcomeChannelId);
 
     if (channel) {
-      await channel.send(
-        `Welcome ${member} to **${member.guild.name}**!`
-      );
+      await channel.send({
+  "content": `Welcome ${member} to **Sea Customs**  — your go-to ER:LC product provider. We offer various servers for you to order from. Order today to receive high-quality products for cheap prices.`,
+  "components": [
+    {
+      "type": 1,
+      "components": [
+        {
+          "style": 2,
+          "type": 2,
+          "label": `${member.guild.memberCount}`,
+          "emoji": {
+            "id": "1487233809614372945",
+            "name": "person",
+            "animated": false
+          },
+          "disabled": true,
+          "flow": {
+            "actions": []
+          },
+          "custom_id": "p_284830800938012674"
+        },
+        {
+          "type": 2,
+          "style": 5,
+          "label": "Dashboard",
+          "url": "https://discord.com/channels/1317959017888354467/1452801176851583048",
+        }
+      ]
+    }
+  ]
+});
     }
 
     client.user.setActivity(`Watching ${member.guild.memberCount} Members`, {
